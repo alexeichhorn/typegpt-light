9562c58823dc15cfced5980568044afb1f51ba73
Alexander Eichhorn <a.eichhorn@losjet.com>
2024-12-21 14:26:20 +0100

    added new gpt-4o model and o1 for structured output compatibility

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -22,7 +22,7 @@ class TestOpenAIChatCompletion:
 
     def test_max_token_counter(self):
         # check if test covers all models (increase if new models are added)
-        assert len(OpenAIChatModel.__args__) == 24  #  type: ignore
+        assert len(OpenAIChatModel.__args__) == 29  #  type: ignore
 
         client = AsyncTypeOpenAI(api_key="mock")
 
@@ -48,7 +48,12 @@ class TestOpenAIChatCompletion:
         assert client.chat.completions.max_tokens_of_model("gpt-4o") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-2024-05-13") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-2024-08-06") == 128_000
+        assert client.chat.completions.max_tokens_of_model("gpt-4o-2024-11-20") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-mini") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-mini-2024-07-18") == 128_000
+        assert client.chat.completions.max_tokens_of_model("o1") == 128_000
+        assert client.chat.completions.max_tokens_of_model("o1-2024-12-17") == 128_000
+        assert client.chat.completions.max_tokens_of_model("o1-mini") == 128_000
+        assert client.chat.completions.max_tokens_of_model("o1-mini-2024-09-12") == 128_000
 
     # -
--- typegpt_light/openai/base_chat_completion.py
+++ typegpt_light/openai/base_chat_completion.py
@@ -29,8 +29,13 @@ class BaseChatCompletions:
                 | "gpt-4o"
                 | "gpt-4o-2024-05-13"
                 | "gpt-4o-2024-08-06"
+                | "gpt-4o-2024-11-20"
                 | "gpt-4o-mini"
                 | "gpt-4o-mini-2024-07-18"
+                | "o1"
+                | "o1-2024-12-17"
+                | "o1-mini"
+                | "o1-mini-2024-09-12"
             ):
                 return 128_000
 
